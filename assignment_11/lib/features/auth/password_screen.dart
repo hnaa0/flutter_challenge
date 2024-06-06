1f6de8c125c08a1d89e9ee9bb9d5e7fd246dff32
unknown <hnaa12@daum.net>
2024-06-06 21:29:06 +0900

    Edit Password screen

--- assignment_11/lib/features/auth/password_screen.dart
+++ assignment_11/lib/features/auth/password_screen.dart
@@ -51,111 +51,118 @@ class _PasswordScreenState extends State<PasswordScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const AppBarWidget(
-        leadingType: LeadingType.back,
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: Sizes.size8,
-            horizontal: Sizes.size28,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const ScreenTitle(title: "You'll need a password"),
-              Gaps.v20,
-              Text(
-                "Make sure it's 8 charactrers or more.",
-                style: TextStyle(
-                  color: const Color(ThemeColors.black).withOpacity(0.7),
-                  fontSize: Sizes.size16,
+    return GestureDetector(
+      onTap: () {
+        _focusNode.unfocus();
+      },
+      child: Scaffold(
+        appBar: const AppBarWidget(
+          leadingType: LeadingType.back,
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: Sizes.size8,
+              horizontal: Sizes.size28,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const ScreenTitle(title: "You'll need a password"),
+                Gaps.v20,
+                Text(
+                  "Make sure it's 8 charactrers or more.",
+                  style: TextStyle(
+                    color: const Color(ThemeColors.black).withOpacity(0.7),
+                    fontSize: Sizes.size16,
+                  ),
                 ),
-              ),
-              Gaps.v36,
-              TextField(
-                obscureText: !_isShowed,
-                focusNode: _focusNode,
-                controller: _passwordController,
-                autocorrect: false,
-                decoration: InputDecoration(
-                  label: Text(
-                    "Password",
-                    style: TextStyle(color: Colors.grey.shade800),
-                  ),
-                  hintText: "Password",
-                  enabledBorder: const UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color(ThemeColors.lightGray),
+                Gaps.v36,
+                TextField(
+                  obscureText: !_isShowed,
+                  focusNode: _focusNode,
+                  controller: _passwordController,
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                    label: Text(
+                      "Password",
+                      style: TextStyle(color: Colors.grey.shade800),
                     ),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Theme.of(context).primaryColor,
+                    hintText: "Password",
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(ThemeColors.lightGray),
+                      ),
                     ),
-                  ),
-                  suffixIcon: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      GestureDetector(
-                        onTap: _isEyeTap,
-                        child: const Icon(
-                          FontAwesomeIcons.eyeSlash,
-                          color: Color(
-                            ThemeColors.lightGray,
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    suffixIcon: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        GestureDetector(
+                          onTap: _isEyeTap,
+                          child: Icon(
+                            _isShowed
+                                ? FontAwesomeIcons.eye
+                                : FontAwesomeIcons.eyeSlash,
+                            color: const Color(
+                              ThemeColors.lightGray,
+                            ),
                           ),
                         ),
-                      ),
-                      Gaps.h16,
-                      FieldCheckMark(
-                        switchKey: _password,
-                        valid: _isPasswordValid(),
-                      ),
-                    ],
+                        Gaps.h16,
+                        FieldCheckMark(
+                          switchKey: _password,
+                          valid: _isPasswordValid(),
+                        ),
+                      ],
+                    ),
+                    contentPadding:
+                        const EdgeInsets.symmetric(vertical: Sizes.size8),
                   ),
-                  contentPadding:
-                      const EdgeInsets.symmetric(vertical: Sizes.size8),
+                  cursorColor: const Color(
+                    ThemeColors.twitterBlue,
+                  ),
                 ),
-                cursorColor: const Color(
-                  ThemeColors.twitterBlue,
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: EdgeInsets.only(
-            bottom: _focusNode.hasFocus
-                ? MediaQuery.of(context).viewInsets.bottom
-                : 0),
-        child: BottomAppBar(
-          height: 80,
-          color: const Color(ThemeColors.extraExtraLightGray),
-          elevation: 0,
-          child: GestureDetector(
-            onTap: () {},
-            child: AnimatedContainer(
-              duration: const Duration(milliseconds: 150),
-              alignment: Alignment.center,
-              width: MediaQuery.of(context).size.width * 0.8,
-              padding: const EdgeInsets.symmetric(
-                vertical: Sizes.size16,
-                horizontal: Sizes.size28,
-              ),
-              decoration: BoxDecoration(
-                color: _isPasswordValid()
-                    ? const Color(ThemeColors.black)
-                    : const Color(ThemeColors.lightGray),
-                borderRadius: BorderRadius.circular(Sizes.size28),
-              ),
-              child: const Text(
-                "Next",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                  fontSize: Sizes.size18,
+        bottomNavigationBar: Padding(
+          padding: EdgeInsets.only(
+              bottom: _focusNode.hasFocus
+                  ? MediaQuery.of(context).viewInsets.bottom
+                  : 0),
+          child: BottomAppBar(
+            height: 80,
+            color: const Color(ThemeColors.extraExtraLightGray),
+            elevation: 0,
+            child: GestureDetector(
+              onTap: () {},
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 150),
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width * 0.8,
+                padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size16,
+                  horizontal: Sizes.size28,
+                ),
+                decoration: BoxDecoration(
+                  color: _isPasswordValid()
+                      ? const Color(ThemeColors.black)
+                      : const Color(ThemeColors.lightGray),
+                  borderRadius: BorderRadius.circular(Sizes.size28),
+                ),
+                child: const Text(
+                  "Next",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: Sizes.size18,
+                  ),
                 ),
               ),
             ),
