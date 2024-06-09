dd45b87898c62ea39ce9de3b37baacccf05f0b81
unknown <hnaa12@daum.net>
2024-06-09 17:27:38 +0900

    Edit create account signup screen

--- assignment_11/lib/features/auth/screens/create_account_signup_screen.dart
+++ assignment_11/lib/features/auth/screens/create_account_signup_screen.dart
@@ -169,91 +169,57 @@ class CreateAccountSignupScreen extends StatelessWidget {
               ),
               Column(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      bottom: Sizes.size12,
-                    ),
-                    child: RichText(
-                      text: TextSpan(
-                        style: TextStyle(
-                          fontFamily: "montserrat",
-                          color:
-                              const Color(ThemeColors.black).withOpacity(0.7),
-                          height: 1.5,
-                        ),
-                        children: [
-                          const TextSpan(
-                              text: "By signning up, you agree to our "),
-                          TextSpan(
-                            text: "Terms",
-                            style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                            ),
-                          ),
-                          const TextSpan(text: ", "),
-                          TextSpan(
-                            text: "Privacy Policy",
-                            style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                            ),
-                          ),
-                          const TextSpan(text: ", and "),
-                          TextSpan(
-                            text: "Cookie Use",
-                            style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                            ),
-                          ),
-                          const TextSpan(
-                              text:
-                                  ". Twitter may use your contact information, including your email address and phone number for purposes outlined in our Privacy Policy. "),
-                          TextSpan(
-                            text: "Learn more",
-                            style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                            ),
-                          ),
-                          const TextSpan(
-                              text:
-                                  ". Others will be able to find you by emial or phone number, when provided unless you choose otherwise"),
-                          TextSpan(
-                            text: " here",
-                            style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                            ),
-                          ),
-                        ],
+                  RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                        fontFamily: "montserrat",
+                        color: const Color(ThemeColors.black).withOpacity(0.7),
+                        height: 1.5,
                       ),
+                      children: [
+                        const TextSpan(
+                            text: "By signning up, you agree to our "),
+                        TextSpan(
+                          text: "Terms",
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                        const TextSpan(text: ", "),
+                        TextSpan(
+                          text: "Privacy Policy",
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                        const TextSpan(text: ", and "),
+                        TextSpan(
+                          text: "Cookie Use",
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                        const TextSpan(
+                            text:
+                                ". Twitter may use your contact information, including your email address and phone number for purposes outlined in our Privacy Policy. "),
+                        TextSpan(
+                          text: "Learn more",
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                        const TextSpan(
+                            text:
+                                ". Others will be able to find you by emial or phone number, when provided unless you choose otherwise"),
+                        TextSpan(
+                          text: " here",
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  // Row(
-                  //   mainAxisAlignment: MainAxisAlignment.center,
-                  //   children: [
-                  //     GestureDetector(
-                  //       onTap: () => _onSignUpTap(context),
-                  //       child: Container(
-                  //         alignment: Alignment.center,
-                  //         width: MediaQuery.of(context).size.width * 0.8,
-                  //         padding: const EdgeInsets.symmetric(
-                  //           vertical: Sizes.size16,
-                  //           horizontal: Sizes.size28,
-                  //         ),
-                  //         decoration: BoxDecoration(
-                  //           color: const Color(ThemeColors.twitterBlue),
-                  //           borderRadius: BorderRadius.circular(Sizes.size28),
-                  //         ),
-                  //         child: const Text(
-                  //           "Sign up",
-                  //           style: TextStyle(
-                  //             color: Colors.white,
-                  //             fontWeight: FontWeight.w600,
-                  //             fontSize: Sizes.size18,
-                  //           ),
-                  //         ),
-                  //       ),
-                  //     ),
-                  //   ],
-                  // ),
                 ],
               ),
             ],
