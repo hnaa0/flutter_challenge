1c57540db4f58c19f1dc4550883f1860b3c3262b
unknown <hnaa12@daum.net>
2024-06-18 19:17:18 +0900

    Delete import code

--- assignment_13/lib/features/settings/settings_screen.dart
+++ assignment_13/lib/features/settings/settings_screen.dart
@@ -2,7 +2,9 @@ import 'package:assignment_13/constants/colors.dart';
 import 'package:assignment_13/constants/gaps.dart';
 import 'package:assignment_13/constants/sizes.dart';
 import 'package:assignment_13/features/settings/privacy_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -122,6 +124,71 @@ class SettingsScreen extends StatelessWidget {
               ),
               title: Text(
                 "About",
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      elevation: 0,
+                      backgroundColor: const Color(
+                        ThemeColors.extraExtraLightGray,
+                      ),
+                      title: const Text(
+                        "Would you like to log out of the Threads?",
+                        style: TextStyle(
+                          fontSize: Sizes.size18,
+                        ),
+                      ),
+                      actions: [
+                        Column(
+                          children: [
+                            FractionallySizedBox(
+                              widthFactor: 1,
+                              child: TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text(
+                                  "Log out",
+                                  style: TextStyle(
+                                    color: Colors.red,
+                                    fontSize: Sizes.size16,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            FractionallySizedBox(
+                              widthFactor: 1,
+                              child: TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text(
+                                  "Cancel",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: Sizes.size16,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              title: const Text(
+                "Log out",
+                style: TextStyle(
+                  color: Color(
+                    ThemeColors.twitterBlue,
+                  ),
+                ),
               ),
             ),
           ],
